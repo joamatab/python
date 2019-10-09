2de7b33090cb552088835ad8de15f67ba5b08c07
joaquin <joaquin@mac>
2019-10-09 15:00:43 -0700

    read requirements.txt

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -9,30 +9,31 @@ from setuptools import setup
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
     text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+
+
+def get_install_requires():
+    with open("requirements.txt", "r") as f:
+        return [line.strip() for line in f.readlines() if not line.startswith("-")]
 
 
 setup(
     name="{{ cookiecutter.package_name }}",
     version="{{ cookiecutter.package_version }}",
     url="{{ cookiecutter.package_url }}",
-    license='MIT',
-
+    license="MIT",
     author="{{ cookiecutter.author_name }}",
     author_email="{{ cookiecutter.author_email }}",
-
     description="{{ cookiecutter.package_description }}",
-    long_description=read("README.rst"),
-
-    packages=find_packages(exclude=('tests',)),
-
-    install_requires=[],
-
+    long_description=read("README.md"),
+    packages=find_packages(exclude=("tests",)),
+    install_requires=get_install_requires(),
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
     ],
 )
