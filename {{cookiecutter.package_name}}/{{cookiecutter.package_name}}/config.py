7b2c714728340dac7cdba65e2068d1e8484fc635
joaquin <j>
2020-07-06 22:16:40 -0700

    simpler config without hiyapyco

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config.py
@@ -1,22 +1,9 @@
-"""
-loads a configuration from 3 files, high priority overwrites low priority:
-
-1. A config.yml found in the current working directory (high priority)
-2. ~/.config/{{ cookiecutter.package_name }}.yml specific for the machine
-3. the default config is in this file
-
+""" store configuration
 """
 
 __all__ = ["CONFIG"]
 
 import pathlib
-import logging
-import hiyapyco
-
-
-default_config = """
-keySample: valueSample
-"""
 
 home = pathlib.Path.home()
 cwd = pathlib.Path.cwd()
@@ -28,16 +15,14 @@ config_dir.mkdir(exist_ok=True)
 module_path = pathlib.Path(__file__).parent.absolute()
 repo_path = module_path.parent
 
-CONFIG = hiyapyco.load(
-    str(default_config),
-    str(cwd_config),
-    str(home_config),
-    failonmissingfiles=False,
-    loglevelmissingfiles=logging.DEBUG,
-)
-CONFIG["module_path"] = module_path
-CONFIG["repo_path"] = repo_path
 
+class Config:
+    module = module_path
+    repo = repo_path
+
+
+CONFIG = Config()
 
 if __name__ == "__main__":
-    print(CONFIG["repo_path"])
+    print(CONFIG)
+
