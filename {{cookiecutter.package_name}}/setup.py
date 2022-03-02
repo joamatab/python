7b6db533bfceea5297c088276d9807ac4ff9bf45
joaquin <j>
2022-03-01 19:56:17 -0800

    fix long_description

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -1,16 +1,10 @@
-import io
-import os
-import re
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding="utf-8") as fd:
-        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
 
 
 def get_install_requires():
@@ -26,7 +20,8 @@ setup(
     author="{{ cookiecutter.author_name }}",
     author_email="{{ cookiecutter.author_email }}",
     description="{{ cookiecutter.package_description }}",
-    long_description=read("README.md"),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
     install_requires=get_install_requires(),
     python_requires=">=3.6",
