4227d9b30eade3b160952927f7c65b174e0ca5ea
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-08-11 11:44:21 -0700

    update config

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config.py
@@ -6,11 +6,7 @@ import pathlib
 
 home = pathlib.Path.home()
 cwd = pathlib.Path.cwd()
-cwd_config = cwd / "config.yml"
 
-home_config = home / ".config" / "{{ cookiecutter.package_name }}.yml"
-config_dir = home / ".config"
-config_dir.mkdir(exist_ok=True)
 module_path = pathlib.Path(__file__).parent.absolute()
 repo_path = module_path.parent
 
@@ -18,10 +14,8 @@ repo_path = module_path.parent
 class Path:
     module = module_path
     repo = repo_path
+    cells = module / "cells"
 
 
 PATH = Path()
-
-if __name__ == "__main__":
-    print(PATH)
 
